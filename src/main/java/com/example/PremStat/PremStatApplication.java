f1f20b76edbbf068ec365e74ffadd8b6d7615381
JeffTunner <abhinavkaushik05@gmail.com>
2026-03-05 15:25:23 +0530

    Initial Commit

---
+++ src/main/java/com/example/PremStat/PremStatApplication.java
@@ -0,0 +1,13 @@
+package com.example.PremStat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PremStatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PremStatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/PremStat/PremStatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PremStat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PremStatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
